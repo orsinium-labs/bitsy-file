e0022576baf6b37f89ac51968f503c353c5d0b31
gram <git@orsinium.dev>
2025-12-15 14:08:30 +0100

    add Error::as_str

--- src/error.rs
+++ src/error.rs
@@ -9,19 +9,21 @@ pub enum NotFound {
     Tile,
 }
 
+impl NotFound {
+    pub fn as_str(&self) -> &'static str {
+        match self {
+            NotFound::Anything => "file is empty",
+            NotFound::Avatar => "avatar not found",
+            NotFound::Room => "room not found",
+            NotFound::Sprite => "sprite not found",
+            NotFound::Tile => "tile not found",
+        }
+    }
+}
+
 impl fmt::Display for NotFound {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(
-            f,
-            "Not found: {} data",
-            match self {
-                NotFound::Anything => "game",
-                NotFound::Avatar => "avatar",
-                NotFound::Room => "room",
-                NotFound::Sprite => "sprite",
-                NotFound::Tile => "tile",
-            }
-        )
+        write!(f, "{}", self.as_str())
     }
 }
 
@@ -46,9 +48,33 @@ pub enum Error {
     Version,
 }
 
+impl Error {
+    pub fn as_str(&self) -> &'static str {
+        match self {
+            Error::Colour => "color error",
+            Error::Dialogue => "dialog error",
+            Error::Ending => "ending error",
+            Error::Exit => "exit error",
+            Error::Font => "font error",
+            Error::Game { missing } => missing.as_str(),
+            Error::Image => "image error",
+            Error::Item => "item error",
+            Error::Palette => "palette error",
+            Error::Position => "position error",
+            Error::Room => "room error",
+            Error::Sprite => "sprite error",
+            Error::Text => "text error",
+            Error::Tile => "tile error",
+            Error::Transition => "transition error",
+            Error::Variable => "variable error",
+            Error::Version => "version error",
+        }
+    }
+}
+
 impl fmt::Display for Error {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "")
+        write!(f, "{}", self.as_str())
     }
 }
 
